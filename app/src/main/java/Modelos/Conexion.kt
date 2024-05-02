f60f3d825f0783a73502bcfad969242380243a17
ethan <20230006@ricaldone.edu.sv>
2024-05-02 15:12:02 -0600

    Proyecto de conexión terminado

---
+++ app/src/main/java/Modelos/Conexion.kt
@@ -0,0 +1,22 @@
+package Modelos
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class Conexion {
+
+    fun cadenaConexion(): Connection?{
+        try {
+            val ip = "jdbc:oracle:thin:@10.10.1.121:1521:xe"
+            val usuario = "system"
+            val contrasena = "desarrollo"
+
+            val conexion = DriverManager.getConnection(ip, usuario, contrasena)
+
+            return conexion
+        }catch (e: Exception){
+            println("El error es este: $e")
+            return null
+        }
+    }
+}

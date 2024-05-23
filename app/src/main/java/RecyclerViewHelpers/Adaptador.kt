9553e170ab9370e456dce52ab6857ab782488f3d
sochiiii <20220018@ricaldone.edu.sv>
2024-05-23 15:42:53 -0600

    Lista Finalizada1

--- app/src/main/java/RecyclerViewHelpers/Adaptador.kt
+++ app/src/main/java/RecyclerViewHelpers/Adaptador.kt
@@ -8,6 +8,11 @@ import sofia.palacios.ccrudsofia1.R
 
 class Adaptador(private var Datos: List<ListaProductos>): RecyclerView.Adapter<ViewHolder>() {
 
+    fun actualizarRecyclerView(nuevaLista: List<ListaProductos>){
+        Datos = nuevaLista
+        notifyDataSetChanged() //Notifica que hay datos nuevos
+    }
+
     //Colocar el mouse en la clase en este caso Adaptador y darle clic en implementar miembros
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val vista = LayoutInflater.from(parent.context).inflate(R.layout.activity_item_card, parent, false)
